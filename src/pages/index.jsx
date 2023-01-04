13ba384a1654d94f66a6c4190ca6b49dc7c73154
GustavoMacedoTech <121763412+GustavoMacedoTech@users.noreply.github.com>
2023-01-04 11:58:26 -0300

    feat: Index page done

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import Home from '../styles/pages/home'
+
+const Index = () => {
+  return (
+    <Home>
+      <Header title='Home' />
+      <h2>Limitless paper in a paper World</h2>
+    </Home>
+  )
+}
+
+export default Index
