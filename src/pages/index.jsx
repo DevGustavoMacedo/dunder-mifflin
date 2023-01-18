5b30c90f65b52a51c50d0a8605e265fecda74fde
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-01-18 17:29:03 -0300

    feat: video intro anime

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import Header from '../components/Header'
-import Home from '../styles/pages/home'
+import { Home, Fade } from '../styles/pages/home'
 
 const Index = () => {
+  const [isVideo, setVideo] = useState(true)
+
+  const handleVideo = () => setVideo(!isVideo)
+
   return (
     <Home>
-      <Header title='Home' />
+      <Header title="Home" />
       <h2>Limitless paper in a paper World</h2>
+      <Fade onClick={handleVideo} video={isVideo}>
+        {isVideo && (
+          <iframe
+            src="https://www.youtube.com/embed/e26dGMGdWQ8?controls=0"
+            title="Dunder Mifflin Intro"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+            allowFullScreen
+          />
+        )}
+      </Fade>
     </Home>
   )
 }
