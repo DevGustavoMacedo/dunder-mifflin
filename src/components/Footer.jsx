9e2e546cd680306df94bc8128f51f2cc70a11f43
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-01-19 11:59:48 -0300

    feat: footer component done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { MyFooter } from '../styles/components/footer'
+
+const Footer = () => (
+  <MyFooter>
+    <p>
+      Developed with 🤍 by
+      <span>
+        <a href="https://github.com/devgustavomacedo" target="_blank" rel="noreferrer">
+          @GustavoMacedo
+        </a>
+      </span>
+    </p>
+  </MyFooter>
+)
+
+export default Footer
