175492cb83af92da1a7a268b93d2449479cd2f54
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-01-20 12:11:06 -0300

    feat: footer styles

---
+++ src/styles/components/footer.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const MyFooter = styled.footer`
+  color: ${(props) => props.theme.colors.secondary};
+  background-color: ${(props) => props.theme.colors.primary};
+  height: 50px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  font-size: 0.9rem;
+  font-family: ${(props) => props.theme.fonts.primary};
+  margin-top: 3rem;
+
+  span a {
+    text-decoration: none;
+    margin-left: 5px;
+    color: #adbaca;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 0.7em;
+    margin-top: 1.4rem;
+    height: 30px;
+  }
+`
