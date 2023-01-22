f8f9c38ba4e39956dad2713313da4f1ce9a8fe3b
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-01-22 20:18:45 -0300

    fix: google fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,13 +40,14 @@ export default class MyDocument extends Document {
             content="https://raw.githubusercontent.com/devgustavomacedo/devgustavomacedo/main/public/dunder-mifflin.png"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Anton&family=Lato:wght@100;400;700&display=swap"
             rel="stylesheet"
           />
+<link rel="stylesheet" href="node_modules/lite-youtube-embed/src/lite-yt-embed.css" />
 
+<script src="node_modules/lite-youtube-embed/src/lite-yt-embed.js"></script>
           <link rel="icon" href="/icons/favicon.png" />
         </Head>
         <body>
